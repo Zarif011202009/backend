f21e0af0f2c6f80ca3e8a7c4599cb3ffe285afbf
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-05-03 02:34:58 +0600

    Email unique commit by Zarif

--- migrations/sqls/20230427150304-init-users-up.sql
+++ migrations/sqls/20230427150304-init-users-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(320) NOT NULL,
+    "email" VARCHAR(320) UNIQUE NOT NULL,
     "phone" VARCHAR(20),
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
